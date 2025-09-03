e4b618b188fbdd2980921cd412a1c7a9a97f716f
Arnaldo <tarnaldo88@gmail.com>
2025-09-03 13:18:36 -0700

    setting up loops to malloc token in tokens

--- input_parser.c
+++ input_parser.c
@@ -5,12 +5,32 @@ char** parse_input(char* input)
     size_t buffer_size = MAX_INPUT;
     char** tokens = malloc(buffer_size * sizeof(char*));
     char* token =  NULL;
+    size_t position = 0;
     size_t token_length = 0;
 
     if(!tokens)
     {
         perror("Malloc");
         exit(EXIT_FAILURE);
+    }
+
+    for(size_t i = 0; input[i]; i++)
+    {
+        token = &input[i];
+
+        while(input[i] && input[i] == ' ')
+        {
+            token_length++;
+            i++;
+        }
+
+        tokens[position] = malloc(token_length + 1 * sizeof(char));
+
+        if(!tokens[position])
+        {
+            perror("Malloc");
+            exit(EXIT_FAILURE);
+        }
     }
 
     return tokens;
