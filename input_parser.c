f6c0b3ceb550f964111d0cde7befb1901e3cae4f
Arnaldo <tarnaldo88@gmail.com>
2025-09-03 13:13:27 -0700

    setting up variables for parse_input

--- input_parser.c
+++ input_parser.c
@@ -2,5 +2,16 @@
 
 char** parse_input(char* input)
 {
+    size_t buffer_size = MAX_INPUT;
+    char** tokens = malloc(buffer_size * sizeof(char*));
+    char* token =  NULL;
+    size_t token_length = 0;
 
+    if(!tokens)
+    {
+        perror("Malloc");
+        exit(EXIT_FAILURE);
+    }
+
+    return tokens;
 }
--- torres_shell.h
+++ torres_shell.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_INPUT 1024
 
 char** parse_input(char* input);
