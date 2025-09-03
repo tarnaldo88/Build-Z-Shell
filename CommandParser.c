c519fadf772e2ce66fb72f5dcf62967f0e5f33f9
Arnaldo <tarnaldo88@gmail.com>
2025-09-03 16:22:17 -0700

    creating skeleton for command functions

--- CommandParser.c
+++ CommandParser.c
@@ -1,2 +1,36 @@
 #include "torres_shell.h"
 
+int command_cd(char** args, char* initial_directory)
+{
+
+}
+
+int command_pwd()
+{
+    return 0;
+}
+
+int command_echo(char **args, char **env)
+{
+    return 0;
+}
+
+int command_env(char **env)
+{
+    return 0;
+}
+
+int command_which(char **args, char **env)
+{
+    return 0;
+}
+
+char **command_setenv(char **args, char **env)
+{
+    return NULL;
+}
+
+char **command_unsetenv(char **args, char **env)
+{
+    return NULL;
+}
