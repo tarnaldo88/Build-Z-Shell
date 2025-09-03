cfb9877a3cbfbf79eda1b2015d033b5bc4104151
Arnaldo <tarnaldo88@gmail.com>
2025-09-03 12:53:33 -0700

    setup objectives of project

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//MAIN OBJ of project
+//Shell loop 
+// Input Parsing
+//Command executions 
+// Built-in Commands exp. cd, pwd, echo, env, setenv, unsetenv, which, exit
+//execute external commands
+//manage environment variables
+//manage path
+//Error handling
+
+
+int main(int argc, char**argv, char** env)
+{
+    
+}
