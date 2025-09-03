63fb03549e45f8b5af3d836237686dbd9ba3d6a2
Arnaldo <tarnaldo88@gmail.com>
2025-09-03 14:24:17 -0700

    making helpers files for helper function. First function implemented was string compare function

---
+++ helpers.c
@@ -0,0 +1,13 @@
+#include "torres_shell.h"
+
+// returns 0 if strings are equal. < 0 str1 < str2 , > 0 str1 > str2
+int my_strcmp(const char* str1, const char* str2)
+{
+    while(*str1 && *str1 == *str2)
+    {
+        str1++;
+        str2++;
+    }
+
+    return *(unsigned char*)str1 - *(unsigned char*)str2;
+}
